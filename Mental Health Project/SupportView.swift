733d146e4ae38ea0bb75334d65e0c2593e60f3ce
Christina L'Hommedieu <CELhommedieu@gmail.com>
2023-07-20 14:03:53 -0400

    texting

--- Mental Health Project/SupportView.swift
+++ Mental Health Project/SupportView.swift
@@ -11,8 +11,16 @@ struct SupportView: View {
     @State var phoneNum : String = ""
     @State var message : String = ""
     var body: some View {
-        VStack {
-            Text("hi")
+        VStack{
+            Text("SUPPORT")
+                .font(.largeTitle)
+            Text("Helplines")
+            Link("(Mental Health Emergency Hotline- 988", destination: URL(string: "tel:988")!)
+            Link("(National Alliance on Mental Illness HelpLine", destination: URL(string: "tel:8009506264")!)
+            Link("National Suicide Prevention Lifeline", destination: URL(string: "tel:1-800-273-TALK (8255)")!)
+            Link("Substance Abuse and Mental Health Services Administration", destination: URL(string: "tel:1-800-662-HELP (4357)")!)
+            Link("Family Violence Helpline", destination: URL(string: "tel:1-800-996-6228")!)
+            Link("National Council on Alcoholism & Drug Dependency", destination: URL(string: "tel:8005551212")!)
             Text("Support")
                 .font(.title)
                 .fontWeight(.bold)
@@ -20,14 +28,15 @@ struct SupportView: View {
                 .padding(.all)
             Button(action: { phoneNum = "741741" ; message = "REASON";
                 sendMessage()
-                    }, label: {
-                        Text("Crisis Text Line")
-                            .font(.title)
-                    })
-            Button(action: {phoneNum = "62640";  message = "HELPLINE" ;              sendMessage()                   }, label: {
-                        Text("National Alliance on Mental Illness")
-                            .font(.title)
-                    })
+            }, label: {
+                Text("Crisis Text Line")
+    
+            })
+           // Button(action: {phoneNum = "62640";  message = "HELPLINE" ;              sendMessage()                   }, label: {
+                //Text("National Alliance on Mental Illness")
+                   // .font(.title)
+            //})
+            
         }
     }
     func sendMessage(){
