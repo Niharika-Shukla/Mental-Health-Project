532e7ade7b42016bc08920ec9dcf32112f825ce5
Christina L'Hommedieu <CELhommedieu@gmail.com>
2023-07-20 14:25:12 -0400

    debugged helplines

--- Mental Health Project/SupportView.swift
+++ Mental Health Project/SupportView.swift
@@ -12,34 +12,33 @@ struct SupportView: View {
     @State var message : String = ""
     var body: some View {
         VStack{
-            Text("SUPPORT")
-                .font(.largeTitle)
-            Text("Helplines")
-            Link("(Mental Health Emergency Hotline- 988", destination: URL(string: "tel:988")!)
-            Link("(National Alliance on Mental Illness HelpLine", destination: URL(string: "tel:8009506264")!)
-            Link("National Suicide Prevention Lifeline", destination: URL(string: "tel:1-800-273-TALK (8255)")!)
-            Link("Substance Abuse and Mental Health Services Administration", destination: URL(string: "tel:1-800-662-HELP (4357)")!)
-            Link("Family Violence Helpline", destination: URL(string: "tel:1-800-996-6228")!)
-            Link("National Council on Alcoholism & Drug Dependency", destination: URL(string: "tel:8005551212")!)
             Text("Support")
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(Color("AccentColor"))
                 .padding(.all)
+            Text("Helplines")
+            Link("(Mental Health Emergency Hotline- 988", destination: URL(string: "tel:988")!)
+            Link("(National Alliance on Mental Illness HelpLine", destination: URL(string: "tel:8009506264")!)
+            Link("National Suicide Prevention Lifeline", destination: URL(string: "tel:18002738255")!)
+            Link("Substance Abuse and Mental Health Services Administration", destination: URL(string: "tel:18006624357")!)
+            Link("Family Violence Helpline", destination: URL(string: "tel:18009966228")!)
+            Link("National Council on Alcoholism & Drug Dependency", destination: URL(string: "tel:8005551212")!)
             Button(action: { phoneNum = "741741" ; message = "REASON";
-                sendMessage()
+                sendMessage(phoneNum: phoneNum , message: message)
             }, label: {
                 Text("Crisis Text Line")
-    
+                
             })
-           // Button(action: {phoneNum = "62640";  message = "HELPLINE" ;              sendMessage()                   }, label: {
-                //Text("National Alliance on Mental Illness")
-                   // .font(.title)
+            // Button(action: {phoneNum = "62640";  message = "HELPLINE" ;              sendMessage()                   }, label: {
+            //Text("National Alliance on Mental Illness")
+            // .font(.title)
             //})
             
         }
     }
-    func sendMessage(){
+}
+func sendMessage(phoneNum : String , message : String){
             let sms: String = "sms:\(phoneNum)&body=\(message)"
             let strURL : String = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
             UIApplication.shared.open(URL.init(string:strURL)!, options: [:], completionHandler: nil)
@@ -50,4 +49,4 @@ struct SupportView: View {
             SupportView()
         }
     }
-}
+
