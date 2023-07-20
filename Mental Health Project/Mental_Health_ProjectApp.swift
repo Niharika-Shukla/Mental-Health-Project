75b1de0a7b066d048dcdf9f4e03cd27353092c6a
Niharika <innovation.niharika@gmail.com>
2023-07-20 10:13:10 -0400

    Initial Commit

---
+++ Mental Health Project/Mental_Health_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mental_Health_ProjectApp.swift
+//  Mental Health Project
+//
+//  Created by Niharika Shukla on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Mental_Health_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
